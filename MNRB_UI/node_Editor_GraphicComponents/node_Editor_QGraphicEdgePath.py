897c41c265f653d451299addb2b4205fa500dec7
btwtill <57750780+btwtill@users.noreply.github.com>
2024-11-13 17:13:26 +0100

    variable renaming and comment adding

--- MNRB_UI/node_Editor_GraphicComponents/node_Editor_QGraphicEdgePath.py
+++ MNRB_UI/node_Editor_GraphicComponents/node_Editor_QGraphicEdgePath.py
@@ -22,42 +22,45 @@ class NodeEditor_QGraphicEdgePathBezier(NodeEditor_QGraphicEdgePathBase):
         
         dist = (self.parent.destination_position[0] - self.parent.source_position[0]) * 0.5
 
-        cpxSource = +dist
-        cpxDestination = -dist
-        cpySource = 0
-        cpyDestination = 0
-
-
+        cpx_source = +dist
+        cpx_destination = -dist
+        cpy_source = 0
+        cpy_destination = 0
 
         if self.parent.edge.start_socket is not None:
-
+            
             is_start_socket_input = self.parent.edge.start_socket.is_input
             is_start_socket_output = self.parent.edge.start_socket.is_output
 
+            #if the sockets are not input on the left screen side and output on the right screen side flip the X component of the control points
             if self.parent.source_position[0] > self.parent.destination_position[0] and is_start_socket_output or self.parent.source_position[0] < self.parent.destination_position[0] and is_start_socket_input:
                 
-                cpxDestination *= -1
-                cpxSource *= -1
+                #inverting the X position of the control points
+                cpx_destination *= -1
+                cpx_source *= -1
                 
-                cpyDestination = (
+                cpy_destination = (
                         (self.parent.source_position[1] - self.parent.destination_position[1]) / math.fabs(
                     (self.parent.source_position[1] - self.parent.destination_position[1]) if (self.parent.source_position[1] -
                                                                         self.parent.destination_position[1]) != 0 else 0.00001)
                 ) * EDGE_CP_ROUNDNESS
 
-                cpySource = (
+                cpy_source = (
                         (self.parent.destination_position[1] - self.parent.source_position[1]) / math.fabs(
                     (self.parent.destination_position[1] - self.parent.source_position[1]) if (self.parent.destination_position[1] -
                                                                         self.parent.source_position[1]) != 0 else 0.00001)
                 ) * EDGE_CP_ROUNDNESS
 
+
+        #path start Position
         path = QPainterPath(QPointF(self.parent.source_position[0], self.parent.source_position[1]))
 
+        #adding final position with a cubic interpolation
         path.cubicTo(
-            self.parent.source_position[0] + cpxSource, 
-            self.parent.source_position[1] + cpySource,
-            self.parent.destination_position[0] + cpxDestination, 
-            self.parent.destination_position[1] + cpyDestination,
+            self.parent.source_position[0] + cpx_source, 
+            self.parent.source_position[1] + cpy_source,
+            self.parent.destination_position[0] + cpx_destination, 
+            self.parent.destination_position[1] + cpy_destination,
             self.parent.destination_position[0], 
             self.parent.destination_position[1]
             )
